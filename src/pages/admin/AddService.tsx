8119318ac5b5d76e23dec587a75d13d6e5b2f090
mdsumonali372 <mdsumonali026@gmail.com>
2024-02-02 20:53:14 +0600

    input

--- src/pages/admin/AddService.tsx
+++ src/pages/admin/AddService.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { FormEvent, useState } from "react";
+
 const AddService = () => {
+  const [serviceName, setServiceName] = useState("");
+  const handleAddService = (e: FormEvent) => {
+    e.preventDefault();
+    const serviceData = {
+      name: serviceName,
+      description: "Replace broken screens",
+      devices: ["Smartphone", "Laptop", "Tablet"],
+      price: 100.0,
+    };
+    console.log(serviceData);
+  };
   return (
     <div>
-      <h2>Add Services</h2>
+      <form onClick={handleAddService}>
+        <input type="text" onChange={(e) => setServiceName(e.target.value)} />
+        <Button type="button">Add</Button>
+      </form>
     </div>
   );
 };
