130125935d59aabc0109138ce74cbd9c9799cda2
mdsumonali372 <mdsumonali026@gmail.com>
2024-02-04 15:18:12 +0600

    tanstack query invalidators queries add

--- src/pages/admin/AddService.tsx
+++ src/pages/admin/AddService.tsx
@@ -1,9 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
 import { FormEvent, useState } from "react";
 
 const AddService = () => {
   const [serviceName, setServiceName] = useState("");
-  const handleAddService = (e: FormEvent) => {
+  const { mutateAsync, isError, isSuccess } = useMutation({
+    mutationFn: async (data) => {
+      return await fetch("http://localhost:5000/api/v1/services", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    },
+  });
+
+  console.log({ isError, isSuccess });
+  const handleAddService = async (e: FormEvent) => {
     e.preventDefault();
     const serviceData = {
       name: serviceName,
@@ -12,6 +26,7 @@ const AddService = () => {
       price: 100.0,
     };
     console.log(serviceData);
+    await mutateAsync(serviceData);
   };
   return (
     <div>
--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -11,10 +11,45 @@ import {
 import { Button } from "@/components/ui/button";
 import { Trash2 } from "lucide-react";
 import { useGetServices } from "@/api/admin/service/service.hook";
+import { FormEvent, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const ServiceList = () => {
   const { data: services, isLoading, isError } = useGetServices();
   console.log(services);
+  const [serviceName, setServiceName] = useState("");
+  const queryClient = useQueryClient();
+  const {
+    mutateAsync,
+    isError: errorService,
+    isSuccess,
+  } = useMutation({
+    mutationFn: async (data) => {
+      return await fetch("http://localhost:5000/api/v1/services", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["services"] });
+    },
+  });
+
+  console.log({ errorService, isSuccess });
+  const handleAddService = async (e: FormEvent) => {
+    e.preventDefault();
+    const serviceData = {
+      name: serviceName,
+      description: "Replace broken screens",
+      devices: ["Smartphone", "Laptop", "Tablet"],
+      price: 100.0,
+    };
+    console.log(serviceData);
+    await mutateAsync(serviceData);
+  };
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -24,38 +59,44 @@ const ServiceList = () => {
   }
 
   return (
-    <Container className="mt-20 rounded-2xl p-0 border">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Name</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead className="text-right">Action</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {services.map((service) => (
-            <TableRow key={service.id}>
-              <TableCell className="font-medium">{service.name}</TableCell>
-              <TableCell>{service.description}</TableCell>
-              <TableCell>{service.price}</TableCell>
-              <TableCell className="text-right">
-                <Button variant="destructive">
-                  <Trash2></Trash2>
-                </Button>
-              </TableCell>
+    <>
+      <Container className="mt-20 rounded-2xl p-0 border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead className="text-right">Action</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">{services.length}</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </Container>
+          </TableHeader>
+          <TableBody>
+            {services.map((service) => (
+              <TableRow key={service.id}>
+                <TableCell className="font-medium">{service.name}</TableCell>
+                <TableCell>{service.description}</TableCell>
+                <TableCell>{service.price}</TableCell>
+                <TableCell className="text-right">
+                  <Button variant="destructive">
+                    <Trash2></Trash2>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total</TableCell>
+              <TableCell className="text-right">{services.length}</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </Container>
+      <form onClick={handleAddService}>
+        <input type="text" onChange={(e) => setServiceName(e.target.value)} />
+        <Button type="button">Add</Button>
+      </form>
+    </>
   );
 };
 
